a5d3831b2617b1345a8405730403e113ab0bca0b
Tanzid <mhossain183055@bscse.uiu.ac.bd>
2022-05-21 06:12:56 +0600

    adding payment in patient_profile.php

--- patient_profile.php
+++ patient_profile.php
@@ -559,3 +559,138 @@ h6 {
                                     </div>
                                 </div>
                                 <h6 class="m-b-20 p-b-5 b-b-default f-w-600" style="margin-top:50px;">payment</h6>
+
+
+
+                        <div class="row">
+        <div class="col-lg-4 mb-lg-0 mb-3">
+            <div class="card p-3">
+                <div class="img-box"> <img src="https://www.freepnglogos.com/uploads/visa-logo-download-png-21.png" alt=""> </div>
+                <div class="number"> <label class="fw-bold" for=""> </label> </div>
+                
+            </div>
+        </div>
+        <div class="col-lg-4 mb-lg-0 mb-3">
+            <div class="card p-3">
+                <div class="img-box"> <img src="https://www.freepnglogos.com/uploads/mastercard-png/file-mastercard-logo-svg-wikimedia-commons-4.png" alt=""> </div>
+                <div class="number"> <label class="fw-bold"></label> </div>
+                
+            </div>
+        </div>
+        <div class="col-lg-4 mb-lg-0 mb-3">
+            <div class="card p-3">
+                <div class="img-box"> <img src="https://www.freepnglogos.com/uploads/discover-png-logo/credit-cards-discover-png-logo-4.png" alt=""> </div>
+                <div class="number"> <label class="fw-bold"></label> </div>
+                
+            </div>
+        </div>
+        <div class="col-lg-4 mb-lg-0 mb-3">
+            <div class="card p-3">
+                <div class="img-box"> <a href="https://www.bkash.com/bn/user"><img src="https://www.logo.wine/a/logo/BKash/BKash-bKash2-Logo.wine.svg" alt=""></a> </div>
+                <div class="number"> <label class="fw-bold"></label> </div>
+                
+            </div>
+        </div>
+        <div class="col-lg-4 mb-lg-0 mb-3">
+            <div class="card p-3">
+                <div class="img-box"> <a href="https://vms.mynagad.com:8484/login"><img src="https://download.logo.wine/logo/Nagad/Nagad-Logo.wine.png" alt=""></a> </div>
+                <div class="number"> <label class="fw-bold"></label> </div>
+                
+            </div>
+        </div>
+        <div class="col-lg-4 mb-lg-0 mb-3">
+            <div class="card p-3">
+                <div class="img-box"> <a href="https://rocketaccount.com/#/sign-in?redirect=https:%2F%2Fdashboard.rocketmortgage.com%3Fqls%3DQNS_20180523.0123456789&adobe_mc=MCMID%3D56112574355775187610787236198558883438%7CMCAID%3D%7CMCORGID%3D5D60123F5245B13E0A490D45@AdobeOrg%7CTS%3D1652903275"><img src="https://seeklogo.com/images/D/dutch-bangla-rocket-logo-B4D1CC458D-seeklogo.com.png" alt=""></a> </div>
+                <div class="number"> <label class="fw-bold"></label> </div>
+                
+            </div>
+        </div>
+
+
+
+
+        <div class="col-12 mt-4">
+            <div class="card p-3">
+                <p class="mb-0 fw-bold h4">Payment Methods</p>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="card p-3">
+                <div class="card-body border p-0">
+                    <p> <a class="btn btn-primary w-100 h-100 d-flex align-items-center justify-content-between" data-bs-toggle="collapse" href="https://www.paypal.com/bd/home" role="button" aria-expanded="true" aria-controls="collapseExample"> <span class="fw-bold">PayPal</span> <span class="fab fa-cc-paypal"></span> </a></p>
+                    <div class="collapse p-3 pt-0" id="collapseExample">
+                        <div class="row">
+                            <div class="col-8">
+                                <p class="h4 mb-0">Summary</p>
+                                <p class="mb-0"><span class="fw-bold">Product:</span><span class="c-green">: Name of product</span></p>
+                                <p class="mb-0"><span class="fw-bold">Price:</span><span class="c-green">:$452.90</span></p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body border p-0">
+                    <p> <a class="btn btn-primary p-2 w-100 h-100 d-flex align-items-center justify-content-between" data-bs-toggle="collapse" href="https://www.paypal.com/us/digital-wallet/ways-to-pay/credit-services" role="button" aria-expanded="true" aria-controls="collapseExample"> <span class="fw-bold">Credit Card</span> <span class=""> <span class="fab fa-cc-amex"></span> <span class="fab fa-cc-mastercard"></span> <span class="fab fa-cc-discover"></span> </span> </a> </p>
+                    <div class="collapse show p-3 pt-0" id="collapseExample">
+                        <div class="row">
+                            <div class="col-lg-5 mb-lg-0 mb-3">
+                                <p class="h4 mb-0">Summary</p>
+                                <p class="mb-0"><span class="fw-bold">Product:</span><span class="c-green">: Visiting fee</span> </p>
+                                <p class="mb-0"> <span class="fw-bold">Price:</span> <span class="c-green">:100tk</span> </p>
+                                
+                            </div>
+                            <div class="col-lg-7">
+                                <form action="" class="form">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="form__div"> <input type="text" class="form-control" placeholder=" "> <label for="" class="form__label">Card Number</label> </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="form__div"> <input type="text" class="form-control" placeholder=" "> <label for="" class="form__label">MM / yy</label> </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="form__div"> <input type="password" class="form-control" placeholder=" "> <label for="" class="form__label">cvv code</label> </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form__div"> <input type="text" class="form-control" placeholder=" "> <label for="" class="form__label">name on the card</label> </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="btn btn-primary w-100">Sumbit</div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="btn btn-primary payment"> Make Payment </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+                                <div class="row">
+
+                                    <div class="col-sm-6">
+                                              <button style=" margin-top:70px;padding-left:30px;padding-right:30px;padding-top:10px;padding-bottom:10px; border:none; background-color:#3ad67b;color:#ffffff;"><a title="save" alt="save" onclick="window.print();" target="_blank" style="cursor:pointer; padding:5px;"> Save recept </a></button>
+                                    </div>
+                                </div>
+
+
+
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
