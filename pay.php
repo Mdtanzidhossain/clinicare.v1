50e9699ed195a60eef01f4ec0223e103bbadbd6f
shaon <fshaon183067@bscse.uiu.ac.bd>
2022-05-21 07:19:33 +0600

    adding end part in pay.php file

--- pay.php
+++ pay.php
@@ -22,3 +22,91 @@
 
 			
 	</section><!--  end hero section  -->
+
+
+<section class="listings" style="background-color:white;">
+		<div class="wrapper">
+			<ul class="properties_list">
+				<h3 style="text-decoration: underline">Payments to proceed</h3>
+				
+				<form method="post" style="display: flex;">
+					<!--<table>
+						<tr>
+							<td>MPESA Transaction ID:</td>
+							<td><input type="text" name="mpesa" required></td>
+						</tr>
+						<tr>
+							<td>National ID Number:</td>
+							<td><input type="text" name="id_no" required></td>
+						</tr>
+						
+						<tr>
+							<td colspan="2" style="text-align:right"><input type="submit" name="pay" value="Submit Details"></td>
+						</tr>
+					</table>-->
+					<div class="info">
+					<h3>Location: <input type="text" name="mpesa" required></h3>
+					<h3>Contact No: <input type="text" name="id_no" required></h3>
+					<h3>Bikash Account No: <input type="text" name="bikash" required></h3>
+					<input style="background-color:#e2136e; border: none; padding: 5px 10px 5px 10px; color:white;" type="submit" name="pay" >
+					</div>
+					<div class="bikashQRcode" style="margin-left: 300px; margin-top:-180px ;">
+						<img src="http://localhost/clinicare/ambulance/img/clinicareBikash.jpg">
+					</div>
+				</form>
+				<?php
+						if(isset($_POST['pay'])){
+							include 'includes/config.php';
+							$mpesa = $_POST['mpesa'];
+							$id_no = $_POST['id_no'];
+							$bikash= $_POST['bikash'];
+							
+							$qry = "INSERT INTO ambulance_payment VALUES ('$mpesa','$id_no','$bikash','')";
+							$result = $conn->query($qry);
+							if($result == TRUE){
+								echo "<script type = \"text/javascript\">
+											alert(\"Payment Successfully Done. Wait for Admin Approval\");
+											window.location = (\"wait.php\")
+											</script>";
+							} else{
+								echo "<script type = \"text/javascript\">
+											alert(\"Registration Failed. Try Again\");
+											window.location = (\"pay.php\")
+											</script>";
+							}
+						}
+						
+					  ?>
+			</ul>
+			
+		</div>
+	</section>	<!--  end listing section  -->
+
+	<footer style="background-color:#054D27 ;">
+
+		<div class="copyrights wrapper">
+			Copyright &copy; <?php echo date("Y")?> All Rights Reserved | Designed by CliniCare Team.
+			<br>Hot line: +8801959608444.
+		</div>
+	</footer>
+
+<!-- whats app chat start-->
+
+    <style>
+      .whatsapp_float{
+        position: fixed;
+        bottom: 40px;
+        right: 20px;
+      }
+    </style>
+    <div class="whatsapp_float">
+      
+        <a href="https://wa.me/+8801521581815" target="_blank"><img src="http://localhost/clinicare/Coverimg/whatsapp.png" width="50px" class="whatsapp_float_btn"></a>
+
+    </div>
+
+  <!-- whats app chat end-->
+	
+</body>
+</html>
+
