11588a4b37904f804d0a2c6f5fa4667c830524a3
shaon <fshaon183067@bscse.uiu.ac.bd>
2022-05-21 06:53:39 +0600

    adding heading in status.php file

---
+++ status.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Ambulance</title>
+	<meta charset="utf-8">
+	<meta name="author" content="pixelhint.com">
+	<meta name="description" content="La casa free real state fully responsive html5/css3 home page website template"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+	
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	<style type="text/css">
+		.status{
+			font-size: 20px;
+		}
+	</style>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body style="background-color: #ffffff;">
+
+	<section class="">
+		<?php
+			include 'header.php';
+		?>
